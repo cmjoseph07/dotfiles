2a39b2d0d0811e13dce21b94f8a2f1c08686e111
Chris Joseph <95969273+cmjoseph07@users.noreply.github.com>
2024-04-10 14:39:29 -0500

    Improved terminal theme

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,24 +1,15 @@
 return {
   -- Using lazy.nvim
   {
-    "ribru17/bamboo.nvim",
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("bamboo").setup({
-        transparent = true,
-        highlights = {
-          -- make comments blend nicely with background, similar to other color schemes
-          ["@comment"] = { fg = "$grey" },
-        },
-      })
-      require("bamboo").load()
-    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "bamboo",
+      colorscheme = "nightfly",
     },
   },
 }
